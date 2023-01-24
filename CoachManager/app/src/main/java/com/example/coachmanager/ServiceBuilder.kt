1019b31278f36986e492214cab2dca28933bda0c
Tomi <tomi.cottrelle.developer@gmail.com>
2023-01-24 12:35:16 +0100

    Fix redis for notification management

--- CoachManager/app/src/main/java/com/example/coachmanager/ServiceBuilder.kt
+++ CoachManager/app/src/main/java/com/example/coachmanager/ServiceBuilder.kt
@@ -4,8 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private val URL = "http://192.168.1.23:3005"
-    
+    //private val URL = "http://192.168.1.23:3005"
+    private val URL = "http://172.31.250.105:3005"
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(URL)
         .addConverterFactory(GsonConverterFactory.create())
